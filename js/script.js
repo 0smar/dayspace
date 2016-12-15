d4eae44777e0566128244a656225ada662508671
0smar <osmar.roramas@gmail.com>
2016-12-15 11:28:15 -0600

    Resposiveness and EC

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+window.sr = ScrollReveal();
+sr.reveal('.logo');
+sr.reveal('.big-icon');
+sr.reveal('.phrase');
+sr.reveal('.mocks');
+sr.reveal('.paragraph');
+sr.reveal('.spacer');
+sr.reveal('.mock-form');
+sr.reveal('.form-structure');
+sr.reveal('.appstore');
+sr.reveal('.fb');
+
+smoothScroll.init();
